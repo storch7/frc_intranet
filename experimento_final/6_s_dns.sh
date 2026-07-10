99a2caded38570ed850053dad98a063341dd281a
Guilherme Storch de Oliveira <GSOliveira@brasal.com.br>
2026-07-10 15:02:30 -0300

    [DNS] Correção de domínio

--- experimento_final/6_s_dns.sh
+++ experimento_final/6_s_dns.sh
@@ -4,22 +4,22 @@
 apt update && apt install -y bind9 bind9utils
 
 cat <<EOF >> /etc/bind/named.conf.local
-zone "grupo4.lan" {
+zone "lab.tapingando.com.br" {
     type master;
-    file "/etc/bind/db.grupo4.lan";
+    file "/etc/bind/db.lab.tapingando.com.br";
 };
 EOF
 
-cat <<EOF > /etc/bind/db.grupo4.lan
+cat <<EOF > /etc/bind/db.lab.tapingando.com.br
 \$TTL    604800
-@       IN      SOA     s.grupo4.lan. admin.grupo4.lan. (
+@       IN      SOA     s.lab.tapingando.com.br. admin.lab.tapingando.com.br. (
                               2         ; Serial
                          604800         ; Refresh
                           86400         ; Retry
                         2419200         ; Expire
                          604800 )       ; Negative Cache TTL
 ;
-@       IN      NS      s.grupo4.lan.
+@       IN      NS      s.lab.tapingando.com.br.
 s       IN      A       172.16.0.2
 r1      IN      A       172.16.0.1
 r2      IN      A       192.168.0.1
